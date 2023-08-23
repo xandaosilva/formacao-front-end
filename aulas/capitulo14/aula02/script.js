6302099a9b416f220e2b7be4486567541da14702
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-08-23 00:29:51 -0300

    Introdução ao JavaScript

---
+++ aulas/capitulo14/aula02/script.js
@@ -0,0 +1,17 @@
+const name = "Alexandre";
+const age = 30;
+const fullStackDevelopr = false;
+const numberX = 15;
+const numberY = 12;
+const sum = numberX + numberY;
+
+console.log(typeof name, name);
+console.log(typeof age, age);
+console.log(typeof fullStackDevelopr, fullStackDevelopr);
+console.log(typeof numberX, numberX);
+console.log(typeof numberY, numberY);
+console.log(`${numberX} + ${numberY} = ${numberX + numberY}`);
+console.log(typeof sum, sum);
+console.log(typeof Infinity, typeof -Infinity, typeof NaN);
+console.log(310 === 310);
+console.log(310 % 2 === 0 && 310 % 310 === 0);
