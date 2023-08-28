daafcda1cfe5e3a2034c91f1cbee73fa16dfac55
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-08-28 10:43:50 -0300

    Projeto DevNotes - JavaScript - Exportando arquivo csv

--- projetos/13_dev_notes/assets/js/scripts.js
+++ projetos/13_dev_notes/assets/js/scripts.js
@@ -2,6 +2,7 @@ const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
 const searchInput = document.querySelector("#search-input");
+const exportBtn = document.querySelector("#export-notes");
 
 function showNotes(){
     cleanNotes();
@@ -146,6 +147,20 @@ function searchNotes(search){
     showNotes();
 }
 
+function exportData(){
+    const notes = getNotes();
+    const csvString = [
+        ["Id", "Conteúdo", "Fixado?"],
+        ...notes.map((note) => [note.id, note.content, note.fixed])
+    ].map((e) => e.join(",")).join("\n");
+
+    const element = document.createElement("a");
+    element.href = "data:text/csv;charset=utf-8" + encodeURI(csvString);
+    element.target = "_blank";
+    element.download = "notes.csv";
+    element.click();
+}
+
 addNoteBtn.addEventListener("click", () => addNote());
 
 searchInput.addEventListener("keyup", (e) => {
@@ -157,6 +172,10 @@ noteInput.addEventListener("keydown", (e) => {
     if(e.key === "Enter"){
         addNote();
     }
+});
+
+exportBtn.addEventListener("click", () => {
+    exportData();
 });
 
 showNotes();
