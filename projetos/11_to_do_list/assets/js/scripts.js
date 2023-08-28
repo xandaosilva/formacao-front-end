9a1d5d080f607e9b038a75bd9c05239fa211802c
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-08-28 00:14:50 -0300

    Projeto To Do List - Js - Adicionando tarefa na lista

--- projetos/11_to_do_list/assets/js/scripts.js
+++ projetos/11_to_do_list/assets/js/scripts.js
@@ -1,1 +1,44 @@
-console.log("Teste");
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+const createBtn = (element, className, icon) => {
+    const btn = document.createElement(element);
+    btn.classList.add(className);
+    btn.innerHTML = icon;
+    return btn;
+}
+
+const saveTodo = (text) => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    const doneBtn = createBtn("button", "finish-todo", '<i class="fa-solid fa-check"><i/>');
+    const editBtn = createBtn("button", "edit-todo", '<i class="fa-solid fa-pen"><i/>');
+    const deleteBtn = createBtn("button", "remove-todo", '<i class="fa-solid fa-xmark"><i/>');
+    
+    todoTitle.innerText = text;
+    
+    todo.appendChild(todoTitle);
+    todo.appendChild(doneBtn);
+    todo.appendChild(editBtn);
+    todo.appendChild(deleteBtn);
+    todoList.appendChild(todo);
+    
+    todoInput.value = "";
+    todoInput.focus();
+}
+
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const inputValue = todoInput.value;
+
+    if(inputValue){
+        saveTodo(inputValue);
+    }
+});
