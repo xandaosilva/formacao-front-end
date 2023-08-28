3f4bca202fce77606fd53c7ab6c4d9b29b166b8f
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-08-28 04:55:21 -0300

    Projeto Gerador de senha - JavaScript - Senha personalizada

--- projetos/12_gerador_de_senhas/assets/js/script.js
+++ projetos/12_gerador_de_senhas/assets/js/script.js
@@ -1,5 +1,12 @@
 const generatePasswordButton = document.querySelector("#generate-password");
 const generatedPasswordelement = document.querySelector("#generated-password");
+const openCloseGeneratorbutton = document.querySelector("#open-generate-password");
+const generatePasswordContainer = document.querySelector("#generate-options");
+const lengthInput = document.querySelector("#length");
+const lettersInput = document.querySelector("#letters");
+const numbersInput = document.querySelector("#numbers");
+const symbolsInput = document.querySelector("#symbols");
+const copyPassword = document.querySelector("#copy-password");
 
 const getLetterLowerCase = () => String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 const getLetterUpperCase = () => String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -11,10 +18,26 @@ const getSymbol = () => {
 
 const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
     let password = "";
-    const passwordLength = 10;
-    const generators = [getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol];
+    const passwordLength = +lengthInput.value;
+    const generators = [];
 
-    for(let i = 0; i < passwordLength; i += 4){
+    if(lettersInput.checked){
+        generators.push(getLetterLowerCase, getLetterUpperCase);
+    }
+
+    if(numbersInput.checked){
+        generators.push(getNumber);
+    }
+
+    if(symbolsInput.checked){
+        generators.push(getSymbol);
+    }
+
+    if(generators.length === 0){
+        return;
+    }
+
+    for(let i = 0; i < passwordLength; i += generators.length){
         generators.forEach(() => {
             const randomValue = generators[Math.floor(Math.random() * generators.length)]();
             password += randomValue;
@@ -28,4 +51,8 @@ const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, get
 
 generatePasswordButton.addEventListener("click", () => {
     generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
+});
+
+openCloseGeneratorbutton.addEventListener("click", () => {
+    generatePasswordContainer.classList.toggle("hide");
 });
