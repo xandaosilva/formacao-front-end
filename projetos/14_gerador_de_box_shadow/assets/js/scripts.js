bd6fc68a238a13b5f6aeb76c3ad8aa65e81519cd
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-08-28 22:24:23 -0300

    Projeto gerador de box shadow - JavaScript - Eventos para alteração da sombra

--- projetos/14_gerador_de_box_shadow/assets/js/scripts.js
+++ projetos/14_gerador_de_box_shadow/assets/js/scripts.js
@@ -33,6 +33,28 @@ class BoxShadowGenerator{
         this.webkitRule.innerText = this.currentRule;
         this.mozRule.innerText = this.currentRule;
     }
+
+    updateValue(type, value){
+        switch (type) {
+            case "horizontal":
+                this.horizontalRef.value = value;
+                break;
+            case "vertical":
+                this.verticalRef.value = value;
+                break;
+            case "blur":
+                this.blurRef.value = value;
+                break;
+            case "spread":
+                this.spreadRef.value = value;
+                break;
+            default:
+                break;
+        }
+
+        this.applyRule();
+        this.showRule();
+    }
 }
 
 const horizontal = document.querySelector("#horizontal");
@@ -51,3 +73,23 @@ const mozRule = document.querySelector("#moz-rule span");
 const boxShadow = new BoxShadowGenerator(horizontal, horizontalRef, vertical, verticalRef, blur, blurRef, spread, spreadRef, previewBox, rule, webkitRule, mozRule);
 
 boxShadow.initialize();
+
+horizontal.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("horizontal", value);
+});
+
+vertical.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("vertical", value);
+});
+
+blur.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("blur", value);
+});
+
+spread.addEventListener("input", (e) => {
+    const value = e.target.value;
+    boxShadow.updateValue("spread", value);
+});
