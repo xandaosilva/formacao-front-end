bd96d9ed8f848c516a169534410c9829447391ce
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-01 00:11:14 -0300

    Projeto Calculadora IMC - Função para limpar formulário

--- projetos/15_calculadora_imc/src/components/Button.jsx
+++ projetos/15_calculadora_imc/src/components/Button.jsx
@@ -1,10 +1,14 @@
 import './Button.css'
 
 // eslint-disable-next-line react/prop-types
-const Button = ({id, text}) => {
-  return (
-    <button id={id}>{text}</button>
-  )
+const Button = ({id, text, action}) => {
+    const handleAction = (e) => {
+        action(e);
+    }
+
+    return (
+        <button onClick={handleAction} id={id}>{text}</button>
+    )
 }
 
 export default Button
