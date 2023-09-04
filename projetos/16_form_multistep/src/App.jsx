ea25b5305b75b4483df1dae20749d7dd18f22eef
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-04 09:05:49 -0300

    Projeto Form multistep - Estrutura componente inicial

--- projetos/16_form_multistep/src/App.jsx
+++ projetos/16_form_multistep/src/App.jsx
@@ -1,10 +1,31 @@
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Form Multistep</h1>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+      </div>
+      <div className="form-container">
+        <p>Etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
