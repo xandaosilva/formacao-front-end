d8e7630040ef5955f2aafa47272a48270057faea
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-04 10:24:55 -0300

    Projeto Form multistep - Funcionalidade para mudar de etapa

--- projetos/16_form_multistep/src/App.jsx
+++ projetos/16_form_multistep/src/App.jsx
@@ -1,4 +1,5 @@
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import { FiSend } from 'react-icons/fi'
 import UserForm from './components/UserForm'
 import ReviewForm from './components/ReviewForm'
 import Thanks from './components/Thanks'
@@ -9,7 +10,7 @@ import './App.css'
 
 function App() {
   const formComponents = [ <UserForm key={0} />, <ReviewForm key={1} />, <Thanks key={2} /> ]
-  const { currentStep, currentComponent } = useForm(formComponents)
+  const { currentStep, currentComponent, changeStep, isFirstStep, isLastStep } = useForm(formComponents)
 
   return (
     <div className="App">
@@ -19,17 +20,31 @@ function App() {
       </div>
       <div className="form-container">
         <p>Etapas</p>
-        <form>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type="button">
-              <GrFormPrevious />
-              <span>Voltar</span>
-            </button>
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            {!isFirstStep && 
+              (
+                <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                  <GrFormPrevious />
+                  <span>Voltar</span>
+                </button>
+              )
+            }
+            {!isLastStep ? 
+              (
+                <button type="submit">
+                  <span>Avançar</span>
+                  <GrFormNext />
+                </button>
+              ) : 
+              (
+                <button type="button">
+                  <span>Enviar</span>
+                  <FiSend />
+                </button>
+              )
+            }
           </div>
         </form>
       </div>
--- projetos/16_form_multistep/src/hooks/useForm.jsx
+++ projetos/16_form_multistep/src/hooks/useForm.jsx
@@ -3,8 +3,17 @@ import { useState } from "react";
 export function useForm(steps){
     const [currentStep, setCurrentStep] = useState(0);
 
+    function changeStep(i, e){
+        if(e) e.preventDefault();
+        if(i < 0 || i >= steps.length) return;
+        setCurrentStep(i);
+    }
+
     return { 
         currentStep,
-        currentComponent: steps[currentStep]
+        currentComponent: steps[currentStep],
+        changeStep,
+        isFirstStep: currentStep === 0 ? true : false,
+        isLastStep: currentStep + 1 === steps.length ? true : false,
     };
 }
