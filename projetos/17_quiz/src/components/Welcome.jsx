7c937cb145bd042683ff356edde9f61fdf12dd97
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-05 09:21:50 -0300

    Projeto Quiz - Configurando Reducer

--- projetos/17_quiz/src/components/Welcome.jsx
+++ projetos/17_quiz/src/components/Welcome.jsx
@@ -6,13 +6,13 @@ import Quiz from '../img/quiz.svg';
 import './Welcome.css';
 
 const Welcome = () => {
-  const quizState = useContext(QuizContext);
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div id="welcome">
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão para começar:</p>
-        <button>Iniciar</button>
+        <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
         <img src={Quiz} alt="Quiz" />
     </div>
   )
--- projetos/17_quiz/src/context/quiz.jsx
+++ projetos/17_quiz/src/context/quiz.jsx
@@ -1,14 +1,26 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+
+import questions from "../data/questions";
+
+const STAGES = ["Start", "Playing", "End"];
+
+const initialState = {
+    gameStage: STAGES[0],
+    questions
+};
+
+const quizReducer = (state, action) => {
+    switch (action.type) {
+        case "CHANGE_STATE":
+            return state;
+        default:
+            return state;
+    }
+}
 
 export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
-    const value = { name: "Quiz"};
+    const value = useReducer(quizReducer, initialState);
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>
 }
-
-
-
-
-
-
