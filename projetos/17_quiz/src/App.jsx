3cbf7128d8647cd01a63e06a8e08957c35da679d
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-05 08:14:05 -0300

    Projeto Quiz - Html e Css - Componente Welcome

--- projetos/17_quiz/src/App.jsx
+++ projetos/17_quiz/src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import Welcome from './components/Welcome';
+
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Quiz App</h1>
+      <h1>Quiz de Programação</h1>
+      <Welcome />
     </div>
   )
 }
 
-export default App
+export default App;
