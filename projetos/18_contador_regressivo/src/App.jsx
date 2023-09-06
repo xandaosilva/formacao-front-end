4d880ba005818fc0dd508e7babfb2561b8bbda19
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-06 09:07:45 -0300

    Projeto Contador regressivo - Criando Componentes Title e Counter

--- projetos/18_contador_regressivo/src/App.jsx
+++ projetos/18_contador_regressivo/src/App.jsx
@@ -1,12 +1,25 @@
-import './App.css'
+import Counter from "./components/Counter";
+import Title from "./components/Title";
+
+import NewYear from "./assets/newyear.jpg";
+
+import "./App.css";
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Countdown</h1>
+    <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
+      <div className="container">
+        <Title title={"Apocalipse"} />
+        <div className="countdown-container">
+          <Counter title={"Dias"} number={2} />
+          <Counter title={"Horas"} number={18} />
+          <Counter title={"Minutos"} number={20} />
+          <Counter title={"Segundos"} number={13} />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
