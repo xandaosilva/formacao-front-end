bc6d99d12590608125861eb3aee9e300175fbdb1
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-06 10:46:40 -0300

    Projeto Contador regressivo - Componente Home - acessando dados do formulário

--- projetos/18_contador_regressivo/src/routes/Home.jsx
+++ projetos/18_contador_regressivo/src/routes/Home.jsx
@@ -1,25 +1,40 @@
+import { useState } from "react";
+
 import "./Home.css";
 
 const Home = () => {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [image, setImage] = useState();
+  const [color, setColor] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const eventObject = { title, date, image, color };
+
+    console.log(eventObject);
+  }
+
   return (
     <div className="home">
         <h2>Monte a sua contagem regressiva</h2>
-        <form className="countdown-form">
+        <form className="countdown-form" onSubmit={handleSubmit}>
             <label>
                 <span>Título:</span>
-                <input type="text" name="title" placeholder="Digite o título do evento" />
+                <input type="text" name="title" placeholder="Digite o título do evento" onChange={(e) => setTitle(e.target.value)} required />
             </label>
             <label>
                 <span>Data do evento:</span>
-                <input type="date" name="date" />
+                <input type="date" name="date" onChange={(e) => setDate(e.target.value)} required />
             </label>
             <label>
                 <span>Imagem:</span>
-                <input type="text" name="image" placeholder="Insira a URL da imagem" />
+                <input type="text" name="image" placeholder="Insira a URL da imagem" onChange={(e) => setImage(e.target.value)} />
             </label>
             <label>
                 <span>Cor do tema:</span>
-                <input type="color" name="color" />
+                <input type="color" name="color" onChange={(e) => setColor(e.target.value)} required />
             </label>
             <input type="submit" value="Enviar" />
         </form>
