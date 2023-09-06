e270668f5de949d4250c71bf9d37404df4b315c1
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-06 10:32:06 -0300

    Projeto Contador regressivo - Componente Home

--- projetos/18_contador_regressivo/src/routes/Home.jsx
+++ projetos/18_contador_regressivo/src/routes/Home.jsx
@@ -1,6 +1,29 @@
+import "./Home.css";
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+        <h2>Monte a sua contagem regressiva</h2>
+        <form className="countdown-form">
+            <label>
+                <span>Título:</span>
+                <input type="text" name="title" placeholder="Digite o título do evento" />
+            </label>
+            <label>
+                <span>Data do evento:</span>
+                <input type="date" name="date" />
+            </label>
+            <label>
+                <span>Imagem:</span>
+                <input type="text" name="image" placeholder="Insira a URL da imagem" />
+            </label>
+            <label>
+                <span>Cor do tema:</span>
+                <input type="color" name="color" />
+            </label>
+            <input type="submit" value="Enviar" />
+        </form>
+    </div>
   )
 }
 
