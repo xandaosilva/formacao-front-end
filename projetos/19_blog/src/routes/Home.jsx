7e0a1ab90c4e3fe83b010cf65ca5f8e2a33c0c4f
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-10 17:26:50 -0300

    Projeto Blog - Acessando dados da API

--- projetos/19_blog/src/routes/Home.jsx
+++ projetos/19_blog/src/routes/Home.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+
+import { useState, useEffect } from 'react'
+
+import { Link } from 'react-router-dom'
+
+import './Home.css'
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async() => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const data = response;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>Home</div>
   )
