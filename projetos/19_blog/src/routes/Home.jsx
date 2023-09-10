3920a700f982a59defe7fd051838e089692db9db
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-10 17:39:40 -0300

    Projeto Blog - Exibindo posts na Home

--- projetos/19_blog/src/routes/Home.jsx
+++ projetos/19_blog/src/routes/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
   const getPosts = async() => {
     try {
       const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
-      const data = response;
-      console.log(data);
+      const data = response.data;
+      setPosts(data);
     } catch (error) {
       console.log(error);
     }
@@ -24,7 +24,18 @@ const Home = () => {
   }, []);
 
   return (
-    <div>Home</div>
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (<p>Carregando posts</p>) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
 
