ad2b57bd7b0e8362f3575f3c9a4efc8a946c9332
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-10 18:41:27 -0300

    Projeto Blog - Axios baseURL

--- projetos/19_blog/src/routes/Home.jsx
+++ projetos/19_blog/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import blogFetch from '../axios/config'
 
 import { useState, useEffect } from 'react'
 
@@ -11,7 +11,7 @@ const Home = () => {
 
   const getPosts = async() => {
     try {
-      const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const response = await blogFetch.get("/posts");
       const data = response.data;
       setPosts(data);
     } catch (error) {
