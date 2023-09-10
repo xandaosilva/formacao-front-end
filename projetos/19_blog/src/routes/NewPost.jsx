a56d3b33c5a6ab9863ac648b9df5730603a2a6e2
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-10 19:49:49 -0300

    Projeto Blog - Componente NewPost e Css

--- projetos/19_blog/src/routes/NewPost.jsx
+++ projetos/19_blog/src/routes/NewPost.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import './NewPost.css'
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className="new-post">
+      <h2>Inserir novo Post:</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input type="text" name="title" id="title" placeholder="Digite o título do Post" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea name="body" id="body" placeholder="Digite o conteúdo do Post"></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className="btn" />
+      </form>
+    </div>
   )
 }
 
