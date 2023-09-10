00d3dc502fc4b165396589f248fe413b16a6f67b
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-10 20:31:32 -0300

    Projeto Blog - Carregamento individual de Post por id

--- projetos/19_blog/src/main.jsx
+++ projetos/19_blog/src/main.jsx
@@ -7,6 +7,7 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 
 import Home from './routes/Home.jsx'
 import NewPost from './routes/NewPost.jsx'
+import Post from './routes/Post.jsx'
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
       {
         path: "/new",
         element: <NewPost />
+      },
+      {
+        path: "/posts/:id",
+        element: <Post />
       }
     ]
   }
