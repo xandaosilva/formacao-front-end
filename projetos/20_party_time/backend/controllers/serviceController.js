f3917604afa1821a5a8b63260692337d2aca0e72
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 14:07:13 -0300

    Projeto Party - backend - Atualizando serviço por ID

--- projetos/20_party_time/backend/controllers/serviceController.js
+++ projetos/20_party_time/backend/controllers/serviceController.js
@@ -56,6 +56,24 @@ const serviceController = {
         } catch (error) {
             console.log(error);
         }
+    },
+    update: async(req, res) => {        
+        const id = req.params.id;
+        const service = {
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            image: req.body.image
+        };
+
+        const updatedService = await ServiceModel.findByIdAndUpdate(id, service);
+
+        if(!updatedService){
+            res.status(404).json({ msg: "Serviço não encontrado" });
+            return;
+        }
+
+        res.status(200).json({ service, msg: "Serviço atualizado com sucesso" });
     }
 }
 
