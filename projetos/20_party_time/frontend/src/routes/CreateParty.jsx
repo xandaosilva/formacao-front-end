c384f1576c9dc4fd3566194bc4b153a2a6d4dd3d
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 19:10:09 -0300

    Projeto Party - frontend - Componente CreateParty

--- projetos/20_party_time/frontend/src/routes/CreateParty.jsx
+++ projetos/20_party_time/frontend/src/routes/CreateParty.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const CreateParty = () => {
   return (
-    <div>CreateParty</div>
+    <div className="form-page">
+      <h2>Crie a sua próxima festa</h2>
+      <p>Defina o orçamento e escolha os serviços</p>
+      <form>
+        <label>
+          <span>Nome da festa:</span>
+          <input type="text" placeholder="Seja criativo" required />
+        </label>
+        <label>
+          <span>Anfitrião:</span>
+          <input type="text" placeholder="Quem está dando a festa?" required />
+        </label>
+        <label>
+          <span>Descrição:</span>
+          <textarea placeholder="Conte mais sobre a festa..." required></textarea>
+        </label>
+        <label>
+          <span>Orçamento:</span>
+          <input type="number" placeholder="Quanto você pretende investir?" required />
+        </label>
+        <label>
+          <span>Imagem:</span>
+          <input type="text" placeholder="Insira a URL de uma imagem" />
+        </label>
+        <div>
+          <h2>Escolha os serviços</h2>
+          <div className="services-container">
+            <p>Serviços</p>
+          </div>
+        </div>
+        <input type="submit" value="Criar festa" className="btn" />
+      </form>
+    </div>
   )
 }
 
