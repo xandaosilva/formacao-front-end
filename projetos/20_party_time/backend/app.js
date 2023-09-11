b48894d9bf36040537cdbc78f269506a33849fe6
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 10:31:41 -0300

    Projeto Party - backend - criando as estruturas de serviços

--- projetos/20_party_time/backend/app.js
+++ projetos/20_party_time/backend/app.js
@@ -9,6 +9,10 @@ const conn = require("./db/conn");
 
 conn();
 
+const routes = require("./routes/router");
+
+app.use("/api", routes);
+
 app.listen(3000, function(){
     console.log("Servidor online");
 });
