72cfd190e9a72b1113afb9e656894d241f6ce07e
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 18:31:18 -0300

    Projeto Party - frontend - Css do componente Home

--- projetos/20_party_time/frontend/src/routes/Home.jsx
+++ projetos/20_party_time/frontend/src/routes/Home.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 
 import { Link } from "react-router-dom";
 
+import "./Home.css";
+
 const Home = () => {
     const [parties, setParties] = useState(null);
 
