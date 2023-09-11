dd3d3c6e54a8e5b765326e25f2d6cebf633d00d0
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 14:58:21 -0300

    Projeto Party - backend - Listando festa por ID

--- projetos/20_party_time/backend/routes/parties.js
+++ projetos/20_party_time/backend/routes/parties.js
@@ -4,5 +4,6 @@ const partyController = require("../controllers/partyController");
 
 router.route("/parties").post((req, res) => partyController.create(req, res));
 router.route("/parties").get((req, res) => partyController.getAll(req, res));
+router.route("/parties/:id").get((req, res) => partyController.get(req, res));
 
 module.exports = router;
