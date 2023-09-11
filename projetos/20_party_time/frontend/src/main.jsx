68a41d07441e3b22925c6acf9aaad7196fa93c38
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 16:07:37 -0300

    Projeto Party - frontend - configurando o react router

--- projetos/20_party_time/frontend/src/main.jsx
+++ projetos/20_party_time/frontend/src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider  } from 'react-router-dom'
+
+import CreateParty from './routes/CreateParty.jsx'
+import Home from './routes/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/party/new",
+        element: <CreateParty />
+      }
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
