c7d48c21bfba68c8d79547e196abc29aecdbe5c3
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 11:40:35 -0300

    Projeto Party - backend - Listando serviços

--- projetos/20_party_time/backend/controllers/serviceController.js
+++ projetos/20_party_time/backend/controllers/serviceController.js
@@ -16,6 +16,15 @@ const serviceController = {
         } catch (error) {
             console.log(error);
         }
+    },
+    getAll: async(req, res) => {
+        try {
+            const services = await ServiceModel.find();
+
+            res.json({ services });
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
--- projetos/20_party_time/backend/routes/services.js
+++ projetos/20_party_time/backend/routes/services.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const serviceController = require("../controllers/serviceController");
 
 router.route("/services").post((req, res) => serviceController.create(req, res));
+router.route("/services").get((req, res) => serviceController.getAll(req, res));
 
 module.exports = router;
