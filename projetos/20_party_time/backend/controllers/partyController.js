45d832a5e9deff32b193e8c1595c174505d72ae5
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-11 14:51:37 -0300

    Projeto Party - backend - Listando festas

--- projetos/20_party_time/backend/controllers/partyController.js
+++ projetos/20_party_time/backend/controllers/partyController.js
@@ -33,6 +33,15 @@ const partyController = {
         } catch (error) {
             console.log(error);
         }
+    },
+    getAll: async(req, res) => {
+        try {
+            const parties = await PartyModel.find();
+            
+            res.json(parties);
+        } catch (error) {
+            console.log(error);
+        }
     }
 };
 
