cb81371bd597a19c05ac0b95c82e20192885403e
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-14 14:04:17 -0300

    Projeto Memories - frontend - configurando react router

--- projetos/21_memories/frontend/src/App.jsx
+++ projetos/21_memories/frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Memories</h1>
+      <h1>Memorires</h1>
+      <div className="container">
+        <Outlet />
+      </div>
     </div>
   )
 }
--- projetos/21_memories/frontend/src/main.jsx
+++ projetos/21_memories/frontend/src/main.jsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import AddMemory from './routes/AddMemory.jsx'
+import Home from './routes/Home.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/add-memory",
+          element: <AddMemory />
+        }
+      ],
+    },
+  ]
+);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
