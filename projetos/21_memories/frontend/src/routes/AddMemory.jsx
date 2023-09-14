3e9ecb24866432df56c9a695df20f552f3e24f69
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-14 14:49:19 -0300

    Projeto Memories - frontend - configurando axios e criando formulário no componente AddMemory

--- projetos/21_memories/frontend/src/routes/AddMemory.jsx
+++ projetos/21_memories/frontend/src/routes/AddMemory.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from "axios"
+
+import { useState } from "react"
+
+import './AddMemory.css'
 
 const AddMemory = () => {
-  return (
-    <div>AddMemory</div>
-  )
+    return (
+        <div className="add-memory-page">
+            <h2>Crie uma nova memória</h2>
+            <form>
+                <label>
+                    <p>Título:</p>
+                    <input type="text" name="title" id="title" placeholder="Defina um título" />
+                </label>
+                <label>
+                    <p>Descrição:</p>
+                    <textarea name="description" id="description" placeholder="Explique o que aconteceu"></textarea>
+                </label>
+                <label>
+                    <p>Foto:</p>
+                    <input type="file" name="image" id="image" />
+                </label>
+                <input type="submit" className="btn" value="Enviar" />
+            </form>
+        </div>
+    )
 }
 
 export default AddMemory
