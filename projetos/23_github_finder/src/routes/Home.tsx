2099b61ff57c0c82483f992b5b924e6b1828c843
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-15 19:21:09 -0300

    Projeto GitHub Finder - Componente Error

--- projetos/23_github_finder/src/routes/Home.tsx
+++ projetos/23_github_finder/src/routes/Home.tsx
@@ -2,15 +2,26 @@ import { UserProps } from "../types/user"
 
 import { useState } from "react"
 
+import Error from "../components/Error"
 import Search from "../components/Search"
 import User from "../components/User"
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false);
 
     const loadUser = async(userName: string) => {
+        setError(false);
+        setUser(null);
+
         const res = await fetch(`https://api.github.com/users/${userName}`);
         const data = await res.json();
+        
+        if(res.status === 404){
+            setError(true);
+            return;
+        }
+        
         const { avatar_url, login, location, followers, following } = data;
         const userData: UserProps = {
             avatar_url,
@@ -27,6 +38,7 @@ const Home = () => {
         <div>
             <Search loadUser={loadUser} />
             {user && <User {...user} />}
+            {error && <Error />}
         </div>
     )
 }
