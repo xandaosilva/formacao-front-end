993a61debd9f8876fe74fd62b09fc0c23652dd39
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-15 16:37:28 -0300

    Projeto GitHub Finder - configurando router

--- projetos/23_github_finder/src/App.tsx
+++ projetos/23_github_finder/src/App.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <h1>GitHub Finder</h1>
+      <Outlet />
     </div>
   )
 }
