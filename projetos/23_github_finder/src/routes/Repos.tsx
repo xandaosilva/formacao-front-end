c40e339501e02e6a8774f47fba3e10f8d621542e
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-15 21:11:56 -0300

    Projeto GitHub Finder -  Carregando repositórios do usuário

--- projetos/23_github_finder/src/routes/Repos.tsx
+++ projetos/23_github_finder/src/routes/Repos.tsx
@@ -1,12 +1,29 @@
+import { RepoProps } from "../types/repo"
+
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
 
 import BackBtn from "../components/BackBtn"
+import Loader from "../components/Loader"
 
 import classes from "./Repos.module.css"
 
 const Repos = () => {
     const { username } = useParams();
+    const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const loadRepos = async function(username: string){
+            setIsLoading(true);
+            const res = await fetch(`https://api.github.com/users/${username}/repos`);
+            const data = await res.json();
+            setIsLoading(false);
+            console.log(data);
+        };
+
+        loadRepos(username);
+    }, []);
 
     return (
         <div>
---
+++ projetos/23_github_finder/src/types/repo.ts
@@ -0,0 +1,7 @@
+export type RepoProps = {
+    name: string,
+    html_url: string,
+    language: string,
+    stargazes_count: number,
+    forks_count: number
+}
