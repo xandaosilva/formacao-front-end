ff6bef5545e8b404eda7218cfe43362647436536
Alexandre Rogerio <alexandrerogerio@pop-os.localdomain>
2023-09-15 21:46:16 -0300

    Projeto GitHub Finder -  Componente Repos

--- projetos/23_github_finder/src/routes/Repos.tsx
+++ projetos/23_github_finder/src/routes/Repos.tsx
@@ -19,16 +19,30 @@ const Repos = () => {
             const res = await fetch(`https://api.github.com/users/${username}/repos`);
             const data = await res.json();
             setIsLoading(false);
-            console.log(data);
+            setRepos(data);
         };
 
-        loadRepos(username);
+        if(username){
+            loadRepos(username);
+        }
     }, []);
+
+    if(!repos && isLoading){
+        return <Loader />
+    }
 
     return (
         <div>
             <BackBtn />
-            Repos: {username}
+            <h2>Explore os repositórios do usuário: {username}</h2>
+            {repos && repos.length === 0 && <p>Não há repositórios.</p>}
+            {repos && repos.length > 0 && (
+                <div>
+                    {repos.map((repo: RepoProps) => (
+                        <p>{repo.name}</p>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
